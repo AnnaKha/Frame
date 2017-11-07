a68c2cd6230330dbc284127a2894529575ee021d
Yevheniia Kashcheieva <Yevheniia_Kashcheieva@epam.com>
2017-11-07 11:38:42 +0200

    Added base table and base row

---
+++ Core/Tables/RowBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using OpenQA.Selenium;
+
+namespace Core.Tables
+{
+    public class RowBase
+    {
+        private readonly IWebElement _element;
+
+        protected RowBase(IWebElement element)
+        {
+            _element = element;
+        }
+
+        protected string GetTdText(int cellIndex)
+        {
+            var elements = _element.FindElements(By.XPath(".//td"));
+            if (!elements.Any()) return null;
+            var element = elements.ElementAt(cellIndex);
+            //element.ScrollTo();
+
+            return element.Text;
+        }
+    }
+}
---
+++ Core/Tables/TableBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.DriverCore;
+using OpenQA.Selenium;
+
+namespace Core.Tables
+{
+    public abstract class TableBase<T> where T : RowBase
+    {
+        protected abstract string Row { get; }
+
+        public IEnumerable<T> Rows => Driver.Instance.FindElements(By.XPath(Row)).Select(CreateInstance<T>).ToList();
+
+        private TRow CreateInstance<TRow>(IWebElement element) where TRow : RowBase
+        {
+            return Activator.CreateInstance(typeof(TRow), element) as TRow;
+        }
+    }
+}
