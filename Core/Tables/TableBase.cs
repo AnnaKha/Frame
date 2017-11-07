2527ed28c88d9421e0e5ed45643be97e462113db
Anna Kharlamova <anchoys07@bk.ru>
2017-11-07 17:04:52 +0200

    merge

--- Core/Tables/TableBase.cs
+++ Core/Tables/TableBase.cs
@@ -6,15 +6,15 @@ using OpenQA.Selenium;
 
 namespace Core.Tables
 {
-    public abstract class TableBase<T> where T : RowBase
-    {
-        protected abstract string Row { get; }
+	public abstract class TableBase<T> where T : RowBase
+	{
+		protected abstract string Row { get; }
 
-        public IEnumerable<T> Rows => Driver.Instance.FindElements(By.XPath(Row)).Select(CreateInstance<T>).ToList();
+		public IEnumerable<T> Rows => Driver.Instance.FindElements(By.XPath(Row)).Select(CreateInstance<T>).ToList();
 
-        private TRow CreateInstance<TRow>(IWebElement element) where TRow : RowBase
-        {
-            return Activator.CreateInstance(typeof(TRow), element) as TRow;
-        }
-    }
+		private TRow CreateInstance<TRow>(IWebElement element) where TRow : RowBase
+		{
+			return Activator.CreateInstance(typeof(TRow), element) as TRow;
+		}
+	}
 }
